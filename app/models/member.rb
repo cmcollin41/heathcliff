d2968ca3ed742b0b5b31a9812ea19103b5fc05e9
Chris Collinsworth <cmcollin41@gmail.com>
2016-06-04 09:59:23 -0700

    added form validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,4 +8,11 @@ class Member < ActiveRecord::Base
   has_many :prompts, through: :assignments
 
   enum member_type: [:writer, :business, :editor, :admin]
+
+	validates_presence_of :first_name
+	validates_presence_of :last_name
+	validates_presence_of :email
+	validates_presence_of :member_type
+	validates_presence_of :password
+	validates_presence_of :password_confirmation
 end
