794927303d932fa84fc39ea61d3d7517469f13fe
Chris Collinsworth <cmcollin41@gmail.com>
2016-06-08 00:29:17 -0600

    added strip sk

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,4 @@ Rails.configuration.stripe = {
     :stripe_secret_key      => ENV[STRIPE_SECRET_KEY]
 }
 
-Stripe.api_key = Rails.application.secrets.stripe[:secret_key]
+Stripe.api_key = ENV[STRIPE_SECRET_KEY]
